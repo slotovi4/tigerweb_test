46eb9ed2d44524c933277edaa1cc14f8fe86bfc3
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-01 20:27:49 +0300

    update Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,44 +5,76 @@ import { cn } from "@bem-react/classname";
 import "./Header.scss";
 
 class Header extends React.Component {
+  public state = {
+    login: false
+  };
+
   public render() {
     const header = cn("Header");
+    const { login } = this.state;
 
     return (
       <section className={header()}>
-        <div className={header("Container")}>
-          <ul className={header("Menu")}>
-            <li className={header("Link", { active: true })}>
-              <a href="#">Главная</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">О компании</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">Оплата</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">Доставка</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">Гарантия</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">Акции %</a>
-            </li>
-            <li className={header("Link")}>
-              <a href="#">Контакты</a>
-            </li>
-          </ul>
+        <div className={header("HeaderBg")}>
+          <div className={header("Container", { type: "menu" })}>
+            <ul className={header("Menu")}>
+              <li className={header("Link", { active: true })}>Главная</li>
+              <li className={header("Link")}>О компании</li>
+              <li className={header("Link")}>Оплата</li>
+              <li className={header("Link")}>Доставка</li>
+              <li className={header("Link")}>Гарантия</li>
+              <li className={header("Link")}>Акции %</li>
+              <li className={header("Link")}>Контакты</li>
+            </ul>
 
-          <ul className={header("Menu")}>
-            <li className={header("Link")}>
-              <a href="#">Регистрация</a>
-            </li>
-            <li className={header("Link", { type: "login" })}>
-              <a href="#">Войти</a>{" "}
-            </li>
-          </ul>
+            <ul className={header("Menu")}>
+              {!login ? (
+                <li className={header("Link")}>Регистрация</li>
+              ) : (
+                <li className={header("Link", { type: "username" })}>
+                  Иван Иванов
+                </li>
+              )}
+
+              <li
+                className={header("Link")}
+                onClick={() => this.setState({ login: !login })}
+              >
+                {login ? "Выйти" : "Войти"}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={header("Container", { type: "info" })}>
+          <div className={header("InfoSection", { type: "logo" })}>
+            <h2 className={header("Logo")}>ювента-крым</h2>
+            <span className={header("PreLogo")}>
+              Официальный дистрибьютер в Крыму с 2005 года
+            </span>
+          </div>
+          <div className={header("InfoSection", { type: "city" })}>
+            <span className={header("Text")}>Ближайший магазин</span>
+            <span className={header("Text", { bold: true })}>Симферополь</span>
+            <span className={header("Text")}>Ул. Маяковского 14-Э</span>
+          </div>
+          <div className={header("InfoSection", { type: "time" })}>
+            <span className={header("Text")}>Пн. - Пт.:</span>
+            <span className={header("Text", { bold: true })}>
+              09.00 - 18.00
+            </span>
+            <span className={header("Text")}>Выходные:</span>
+            <span className={header("Text")}>Cб. - Вс.</span>
+          </div>
+          <div className={header("InfoSection", { type: "tel" })}>
+            <a className={header("Text")} href="tel:+79787290215">
+              +7(978) <b>729-02-15</b>
+            </a>
+            <a className={header("Text")} href="tel:3652248231">
+              (3652) <b>24-82-31</b>
+            </a>
+            <span className={header("Call")}>обратный звонок</span>
+          </div>
         </div>
       </section>
     );
