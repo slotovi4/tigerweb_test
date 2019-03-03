fd2ed2dce9c53c0bd7682181730af61840ebf9f2
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-03 20:47:48 +0300

    void

--- build/request/sendItemRate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$vote_average = $_POST['vote_average'];
-
-$send = mail("roma.bruns2012@gmail.com", "Оценка продукта", "Оценка:".$vote_average);
-if ($send) {  
-  return true;
-} else { 
-  return false;
-}
--- public/request/sendItemRate.php
+++ public/request/sendItemRate.php
@@ -1,7 +1,7 @@
 <?php
 $vote_average = $_POST['vote_average'];
 
-$send = mail("roma.bruns2012@gmail.com", "Оценка продукта", "Оценка:".$vote_average);
+$send = mail("mail@gmail.com", "Оценка продукта", "Оценка:".$vote_average);
 if ($send) {  
   return true;
 } else { 
